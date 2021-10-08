7a15f85191b5acb864b0004932896b647e0041c5
Alessandro Pangrazio <alessandropangrazio@live.com>
2021-10-08 15:08:07 +1030

    getting there

--- int_uint.c
+++ int_uint.c
@@ -12,4 +12,26 @@ void	isint(t_bag arg_count)
 	free(str);
 }
 
+void	isun_int(t_bag *args)
+{
+	char	*str;
+	int	value;
 
+	value = va_arg(arg_count->args, unsigned int)
+	str = uitoa(value);
+	ft_putstr_fd(str, 1);
+	arg_count->counter += ft_strlen(str);
+	free(str);
+}
+
+static char	*uitoa(unsigned int n)
+{
+	char	*str
+	int	len;
+
+	len = numcounter;
+	str = (char *)malloc(len + 1 sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+}
